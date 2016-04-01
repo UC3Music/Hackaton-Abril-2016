f1dd8cc12c335a78e3693870a0a710a9a083571c
jgvictores <jcgvicto@gmail.com>
2016-04-01 14:17:59 +0200

    only one buttonState

--- buttones/buttones.ino
+++ buttones/buttones.ino
@@ -7,7 +7,7 @@ int notes[] = { NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4 };
 // los pin numbers EL TAMANO TIENE QUE SER IQUAL QUE LO DE notes
 int buttonPins[] = { 2, 3, 4, 5, 6, 7, 8 };
 // los estados de los bottones (indeces corresponden a los de buttonPins)
-int buttonStates[] = { 0, 0, 0, 0, 0, 0, 0 };
+int buttonState = 0;
 
 // el pin del altavoz
 int altavozPins[] = { 9, 10 };
@@ -27,9 +27,9 @@ void loop() {
   altavozUno = false; 
   
   for (int i = 0; i < 7; i++) { // el tamano de notes
-    buttonStates[i] = digitalRead(buttonPins[i]);
+    buttonState = digitalRead(buttonPins[i]);
 
-    if (buttonStates[i] == HIGH) {
+    if (buttonState == HIGH) {
       if (altavozUno) {
         tone(altavozPins[0], notes[i],20);
         altavozUno = true;
@@ -37,8 +37,6 @@ void loop() {
       else {
         tone(altavozPins[1], notes[i],20);
       }    
-    } else {
-      //noTone(altavozPin);  // messes things up
     }
     
   }
