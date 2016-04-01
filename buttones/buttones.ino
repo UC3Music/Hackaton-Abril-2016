2bff4594ad91e78e4aa05ece00a339346fa104f7
jgvictores <jcgvicto@gmail.com>
2016-04-01 13:35:47 +0200

    avoid using noTone

--- buttones/buttones.ino
+++ buttones/buttones.ino
@@ -3,7 +3,7 @@
 
 
 // los tonos 
-int notes[] = { NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_G4 }; 
+int notes[] = { NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4 };
 
 // los pin numbers EL TAMANO TIENE QUE SER IQUAL QUE LO DE notes
 int buttonPins[] = { 2, 3, 4, 5, 6, 7, 8 };
@@ -26,9 +26,9 @@ void loop() {
     buttonStates[i] = digitalRead(buttonPins[i]);
 
     if (buttonStates[i] == HIGH) {
-      tone(altavozPin, notes[i]);
+      tone(altavozPin, notes[i],50);
     } else {
-      noTone(altavozPin);
+      //noTone(altavozPin);  // messes things up
     }
     
   }
