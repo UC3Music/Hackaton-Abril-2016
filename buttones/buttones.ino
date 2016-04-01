9f6a436eed001e61822152882e2a0c01567c8ef3
Emily J Beckers <emilyjbeckers@wifi-75-252.uc3m.es>
2016-04-01 14:09:25 +0200

    add buttones

---
+++ buttones/buttones.ino
@@ -0,0 +1,36 @@
+#include "pitches.h"
+
+
+
+// los tonos 
+int notes[] = { NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_G4 }; 
+
+// los pin numbers EL TAMANO TIENE QUE SER IQUAL QUE LO DE notes
+int buttonPins[] = { 2, 3, 4, 5, 6, 7, 8 };
+// los estados de los bottones (indeces corresponden a los de buttonPins)
+int buttonStates[] = { 0, 0, 0, 0, 0, 0, 0 };
+
+//el pin del altavoz
+const int altavozPin = 9;
+
+void setup() {
+  for (int i = 0; i < 7; i++) { // el tamano de buttonPins
+    pinMode(buttonPins[i], INPUT);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 7; i++) { // el tamano de notes
+    buttonStates[i] = digitalRead(buttonPins[i]);
+
+    if (buttonStates[i] == HIGH) {
+      tone(altavozPin, notes[i]);
+    } else {
+      noTone(altavozPin);
+    }
+    
+  }
+  
+}
